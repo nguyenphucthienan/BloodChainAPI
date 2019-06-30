e164f02afce7fcd4e47873ba7712d8d680047cb4
Nguyen Phuc Thien An <npta97@gmail.com>
2019-06-30 14:50:53 +0700

    Add Helmet

--- startups/utils.js
+++ startups/utils.js
@@ -1,7 +1,9 @@
 const bodyParser = require('body-parser');
+const helmet = require('helmet');
 const morgan = require('morgan');
 
 module.exports = (app) => {
   app.use(bodyParser.json());
+  app.use(helmet());
   app.use(morgan('dev'));
 };
